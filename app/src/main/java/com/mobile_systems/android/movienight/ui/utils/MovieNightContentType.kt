134c6ffe01c0a145425e853d9eee05904236b864
Elisa <ciardulli.elisa@gmail.com>
2026-02-02 00:51:09 +0100

    finished rafactoring

--- app/src/main/java/com/mobile_systems/android/movienight/ui/utils/MovieNightContentType.kt
+++ app/src/main/java/com/mobile_systems/android/movienight/ui/utils/MovieNightContentType.kt
@@ -1,5 +1,8 @@
 package com.mobile_systems.android.movienight.ui.utils
 
+/**
+ * Defines the layout strategy based on the available screen real estate.
+ */
 enum class MovieNightContentType {
     LIST_ONLY, LIST_AND_DETAIL
 }
