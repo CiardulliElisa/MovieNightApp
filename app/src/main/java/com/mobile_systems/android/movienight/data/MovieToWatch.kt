3cc3c63bbce3d3ecb4dcffa6b11d77fca9667f67
Elisa <ciardulli.elisa@gmail.com>
2026-02-01 23:25:04 +0100

    commnted

--- app/src/main/java/com/mobile_systems/android/movienight/data/MovieToWatch.kt
+++ app/src/main/java/com/mobile_systems/android/movienight/data/MovieToWatch.kt
@@ -3,6 +3,7 @@ package com.mobile_systems.android.movienight.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+//Defines a table in the local database: moviesToWatch and how each entity is defined
 @Entity(tableName = "moviesToWatch")
 data class MovieToWatch (
     @PrimaryKey(autoGenerate = false)
