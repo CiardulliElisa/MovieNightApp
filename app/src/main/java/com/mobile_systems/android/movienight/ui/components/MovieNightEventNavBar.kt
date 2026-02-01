f25b99b6cd0489acf3435744e5e1fc0f3a2d81ea
Elisa <ciardulli.elisa@gmail.com>
2026-02-01 22:34:39 +0100

    commented movienighteventbar

--- app/src/main/java/com/mobile_systems/android/movienight/ui/components/MovieNightEventNavBar.kt
+++ app/src/main/java/com/mobile_systems/android/movienight/ui/components/MovieNightEventNavBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+//A navbar for the movie night event screens, to either go back to the home page or try again and start a new movie night event from scratch
 @Composable
 fun MovieNightEventNavBar(
     onTryAgainClick: () -> Unit,
@@ -17,13 +18,14 @@ fun MovieNightEventNavBar(
     modifier: Modifier = Modifier
 ) {
     NavigationBar(modifier = modifier) {
+        //Home Button
         NavigationBarItem(
             selected = false,
             onClick = onHomeClick,
             label = { Text("Home") },
             icon = { Icon(Icons.Default.Home, contentDescription = "Home") }
         )
-
+        //Try Again Button
         NavigationBarItem(
             selected = false,
             onClick = onTryAgainClick,
