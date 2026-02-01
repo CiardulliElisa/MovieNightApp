50ad0a18faf7b432c882fcfa4db10cbb920e6bc7
Elisa <ciardulli.elisa@gmail.com>
2026-02-01 14:41:02 +0100

    refactored MovieNightApplication

--- app/src/main/java/com/mobile_systems/android/movienight/MovieNightApplication.kt
+++ app/src/main/java/com/mobile_systems/android/movienight/MovieNightApplication.kt
@@ -4,6 +4,10 @@ import android.app.Application
 import com.mobile_systems.android.movienight.data.AppContainer
 import com.mobile_systems.android.movienight.data.AppDataContainer
 
+/*
+* Runs only once when the app is started.
+* It provides dependencies to the rest of the app.
+*/
 class MovieNightApplication : Application() {
     lateinit var container: AppContainer
 
