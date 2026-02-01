782fd1d85e2972a853c0234c5057fd6be62fbcf3
Elisa <ciardulli.elisa@gmail.com>
2026-02-01 23:15:30 +0100

    commneted fried

--- app/src/main/java/com/mobile_systems/android/movienight/data/Friend.kt
+++ app/src/main/java/com/mobile_systems/android/movienight/data/Friend.kt
@@ -3,6 +3,7 @@ package com.mobile_systems.android.movienight.data
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
+//Simple class used throughout the movie night events, to define users participating in the session, by assigning them an icon, a color and a name
 data class Friend(
     val icon: ImageVector,
     val color: Color,
