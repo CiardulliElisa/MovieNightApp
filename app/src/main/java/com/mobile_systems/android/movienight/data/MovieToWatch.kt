12f2888d9332ee1e7bbe28d9fbaa5d2b7c42f826
Elisa <ciardulli.elisa@gmail.com>
2026-02-01 23:27:20 +0100

    commented

--- app/src/main/java/com/mobile_systems/android/movienight/data/MovieToWatch.kt
+++ app/src/main/java/com/mobile_systems/android/movienight/data/MovieToWatch.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 //Defines a table in the local database: moviesToWatch and how each entity is defined
+//Each instance represents a movie through its thumbnail (if available), and its id
 @Entity(tableName = "moviesToWatch")
 data class MovieToWatch (
     @PrimaryKey(autoGenerate = false)
--- app/src/main/java/com/mobile_systems/android/movienight/data/WatchedMovie.kt
+++ app/src/main/java/com/mobile_systems/android/movienight/data/WatchedMovie.kt
@@ -3,6 +3,8 @@ package com.mobile_systems.android.movienight.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+//Defines a table in the local database: watchedMovies and how each entity is defined
+//Each instance represents a movie through its thumbnail (if available), and its id
 @Entity(tableName = "watchedMovies")
 data class WatchedMovie (
     @PrimaryKey(autoGenerate = false)
